7de98b653167addbd273b6228ebdc04d5b469e00
Iqra Qadeer <iqra_afaq@hotmail.com>
2023-05-26 23:52:16 +0200

    2nd commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require("../model/database.js"); // Import the function to interact with the database
 const app = express();
-const port = 3000;
+const port = 5000;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,10 +19,8 @@ router.get('/api/paintings', function(req, res, next) {
     .catch(error => {
       res.status(500).json({ error: 'Internal Server Error' });
     });
-}); // <-- Added closing brace for the route handler
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
 
+
+
+module.exports = router;
